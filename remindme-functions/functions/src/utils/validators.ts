a7ce548a6ac2e5a848f992d5828b2d377f4af391
Baptiste PONTANIER <bpontanier@ciril.lan>
2019-03-26 14:39:56 +0100

    Updates

--- remindme-functions/functions/src/utils/validators.ts
+++
@@ -1,8 +0,0 @@
-export const isEmpty = (str:string):boolean => {
-    return str===undefined || str.trim() === '';
-}
-
-const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const isMail = (str:string):boolean => {
-    return emailRegEx.test(str);
-}
